104b5a9d2e865fe5c4ad6c8c174c9312c13b3986
Kolja Markwardt <vanmar.development@gmail.com>
2013-04-08 11:24:34 +0200

    - added test for ProjectListActivity

---
+++ KnitDroidUnitTest/src/de/vanmar/android/knitdroid/ProjectListActivityUnitTest.java
@@ -0,0 +1,37 @@
+package de.vanmar.android.knitdroid;
+
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.matchers.StartedMatcher;
+
+import android.content.Intent;
+
+@RunWith(RobolectricTestRunner.class)
+public class ProjectListActivityUnitTest {
+
+	private ProjectListActivity activity;
+
+	@Before
+	public void prepare() {
+		activity = new ProjectListActivity();
+		activity.onCreate(null);
+	}
+
+	@Test
+	public void shouldCallProjectActivity() throws Exception {
+		// assume
+		final int projectId = 17;
+
+		// when
+		activity.onProjectSelected(projectId);
+
+		// then
+		final Intent intent = new Intent(activity, ProjectDetailActivity_.class);
+		intent.putExtra(ProjectDetailActivity.EXTRA_PROJECT_ID, projectId);
+		assertThat(activity, new StartedMatcher(intent));
+	}
+}
