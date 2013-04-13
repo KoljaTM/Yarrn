45eb90775a5fa498cadb24fafe30ca9212da47ba
Kolja Markwardt <vanmar.development@gmail.com>
2013-04-13 12:40:52 +0200

    added mocks for oauth authentication process

---
+++ KnitDroidUnitTest/src/de/vanmar/android/knitdroid/util/KnitdroidHttpMock.java
@@ -0,0 +1,65 @@
+package de.vanmar.android.knitdroid.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+public class KnitdroidHttpMock extends NanoHTTPD {
+
+	public KnitdroidHttpMock(final int port, final File wwwroot)
+			throws IOException {
+		super(port, wwwroot);
+	}
+
+	@Override
+	public Response serve(final String uri, final String method,
+			final Properties header, final Properties parms,
+			final Properties files) {
+		final Response response = super
+				.serve(uri, method, header, parms, files);
+		if (uri.startsWith("/oauth/authorize")) {
+			response.mimeType = MIME_HTML;
+			response.addHeader("Content-Type", MIME_HTML);
+			response.header.remove("Content-Disposition");
+		}
+		return response;
+	}
+
+	/**
+	 * Starts as a standalone file server and waits for Enter.
+	 */
+	public static void main(final String[] args) {
+		myOut.println("NanoHTTPD 1.27 (C) 2001,2005-2013 Jarno Elonen and (C) 2010 Konstantinos Togias\n"
+				+ "(Command line options: [-p port] [-d root-dir] [--licence])\nAdapted for Knitdroid testing");
+
+		// Defaults
+		int port = 80;
+		File wwwroot = new File(".").getAbsoluteFile();
+
+		for (int i = 0; i < args.length; ++i) {
+			if (args[i].equalsIgnoreCase("-p")) {
+				port = Integer.parseInt(args[i + 1]);
+			} else if (args[i].equalsIgnoreCase("-d")) {
+				wwwroot = new File(args[i + 1]).getAbsoluteFile();
+			}
+		}
+
+		try {
+			myOut.println("Starting server on port " + port);
+			new KnitdroidHttpMock(port, wwwroot);
+		} catch (final IOException ioe) {
+			myErr.println("Couldn't start server:\n" + ioe);
+			System.exit(-1);
+		}
+
+		myOut.println("Now serving files in port " + port + " from \""
+				+ wwwroot + "\"");
+		myOut.println("Hit Enter to stop.\n");
+
+		try {
+			System.in.read();
+		} catch (final Throwable t) {
+		}
+	}
+
+}
