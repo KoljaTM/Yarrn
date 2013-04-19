f2c39fd4d6526030a8c45ce4fd5c416d97fbd8d6
Kolja <Kolja@VPC>
2013-04-19 22:21:39 +0200

    - Moved PhotoUpload to separate class - Guard against configuration change by making PhotoUploader a NonconfigurationInstance and handling orientationChange in the activity

--- KnitDroid/src/de/vanmar/android/knitdroid/projects/ProjectDetailActivity.java
+++ KnitDroid/src/de/vanmar/android/knitdroid/projects/ProjectDetailActivity.java
@@ -1,8 +1,24 @@
 package de.vanmar.android.knitdroid.projects;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.widget.Toast;
+
+import com.googlecode.androidannotations.annotations.Bean;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.Extra;
 import com.googlecode.androidannotations.annotations.FragmentById;
+import com.googlecode.androidannotations.annotations.NonConfigurationInstance;
+import com.googlecode.androidannotations.annotations.UiThread;
 
 import de.vanmar.android.knitdroid.AbstractRavelryActivity;
 import de.vanmar.android.knitdroid.R;
@@ -14,11 +30,25 @@ public class ProjectDetailActivity extends AbstractRavelryActivity implements
 
 	public static final String EXTRA_PROJECT_ID = "ProjectDetailActivity.extra.project_id";
 
+	private static final int REQUEST_CODE_GALLERY = 1;
+	private static final int REQUEST_CODE_CAMERA = 2;
+
+	private static final String JPEG_FILE_PREFIX = "IMG_";
+
+	private static final String JPEG_FILE_SUFFIX = ".jpg";
+
 	@FragmentById(R.id.projectFragment)
 	ProjectFragment projectFragment;
 
 	@Extra(EXTRA_PROJECT_ID)
 	protected int projectId;
+
+	@Bean
+	@NonConfigurationInstance
+	PhotoUploadTask photoUploadTask;
+
+	@NonConfigurationInstance
+	Uri photoUri;
 
 	@Override
 	protected void onResume() {
@@ -29,5 +59,62 @@ public class ProjectDetailActivity extends AbstractRavelryActivity implements
 
 	public void onProjectSelected(final int projectId) {
 		projectFragment.onProjectSelected(projectId);
+	}
+
+	@Override
+	public void pickImage() {
+		final Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_PICK);
+		intent.setType("image/*");
+		startActivityForResult(intent, REQUEST_CODE_GALLERY);
+	}
+
+	@Override
+	@SuppressLint("SimpleDateFormat")
+	public void takePhoto() {
+		final File storageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				"Knitdroid");
+		storageDir.mkdirs();
+
+		final String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+				.format(new Date());
+		final String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+		File image;
+		try {
+			image = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX,
+					storageDir);
+			final Intent takePictureIntent = new Intent(
+					MediaStore.ACTION_IMAGE_CAPTURE);
+			photoUri = Uri.fromFile(image);
+			takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+			startActivityForResult(takePictureIntent, REQUEST_CODE_CAMERA);
+		} catch (final IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void onActivityResult(final int requestCode, final int resultCode,
+			final Intent data) {
+		if (requestCode == REQUEST_CODE_GALLERY
+				&& resultCode == Activity.RESULT_OK) {
+			uploadPhotoToProject(data.getData(), projectId);
+		} else if (requestCode == REQUEST_CODE_CAMERA
+				&& resultCode == Activity.RESULT_OK && photoUri != null) {
+			uploadPhotoToProject(photoUri, projectId);
+			photoUri = null;
+		}
+	}
+
+	public void uploadPhotoToProject(final Uri photoUri, final int projectId) {
+		photoUploadTask.uploadPhotoToProject(photoUri, projectId);
+	}
+
+	@UiThread
+	public void onPhotoUploadSuccess() {
+		Toast.makeText(this, "Upload successful", Toast.LENGTH_LONG).show();
 	}
 }
