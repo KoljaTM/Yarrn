04a4a1865c91c7ffdd6ea094632f923dad857947
Kolja Markwardt <vanmar.development@gmail.com>
2013-11-11 22:51:40 +0100

    use robospice to handle request lifecycle
    WIP

--- KnitDroid/src/de/vanmar/android/knitdroid/projects/ProjectsFragment.java
+++ KnitDroid/src/de/vanmar/android/knitdroid/projects/ProjectsFragment.java
@@ -1,33 +1,44 @@
 package de.vanmar.android.knitdroid.projects;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.scribe.model.OAuthRequest;
-import org.scribe.model.Verb;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.widget.ListView;
-
-import com.androidquery.util.AQUtility;
-import com.googlecode.androidannotations.annotations.AfterViews;
-import com.googlecode.androidannotations.annotations.Background;
-import com.googlecode.androidannotations.annotations.EFragment;
-import com.googlecode.androidannotations.annotations.UiThread;
-import com.googlecode.androidannotations.annotations.ViewById;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.googlecode.androidannotations.annotations.*;
 import com.googlecode.androidannotations.annotations.sharedpreferences.Pref;
-
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.UncachedSpiceService;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.SpiceRequest;
+import com.octo.android.robospice.request.listener.RequestListener;
 import de.vanmar.android.knitdroid.KnitdroidPrefs_;
 import de.vanmar.android.knitdroid.R;
 import de.vanmar.android.knitdroid.ravelry.IRavelryActivity;
+import de.vanmar.android.knitdroid.ravelry.RavelryApi;
 import de.vanmar.android.knitdroid.ravelry.ResultCallback;
+import de.vanmar.android.knitdroid.ravelry.dts.ProjectResult;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+
+import java.util.concurrent.TimeUnit;
 
 @EFragment(R.layout.fragment_projects)
 public class ProjectsFragment extends Fragment {
 
+	protected SpiceManager spiceManager = new SpiceManager(UncachedSpiceService.class);
+
 	public interface ProjectsFragmentListener extends IRavelryActivity {
-		/** Project with projectId was selected, 0 if no project selected */
+		/**
+		 * Project with projectId was selected, 0 if no project selected
+		 */
 		void onProjectSelected(int projectId);
 	}
 
@@ -97,6 +108,33 @@ public class ProjectsFragment extends Fragment {
 	@Override
 	public void onStart() {
 		super.onStart();
+		spiceManager.start(this.getActivity());
+
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		try {
+			spiceManager.execute(new ProjectsRequest(), new ProjectsListener());
+		} catch (Exception e) {
+			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		} finally {
+		}
+	}
+
+	@Override
+	public void onStop() {
+		spiceManager.shouldStop();
+		super.onStop();
+	}
+
+	/*@Override
+	public void onStart() {
+		super.onStart();
+
+
 		getProjects(new ResultCallback<String>() {
 
 			@Override
@@ -109,5 +147,50 @@ public class ProjectsFragment extends Fragment {
 				displayProjects(result);
 			}
 		});
+	}      */
+
+	class ProjectsRequest extends SpiceRequest<ProjectResult> {
+
+		public ProjectsRequest() {
+			super(ProjectResult.class);
+		}
+
+		@Override
+		public ProjectResult loadDataFromNetwork() throws Exception {
+			final String apiKey = getString(R.string.api_key);
+			final String apiSecret = getString(R.string.api_secret);
+			final String callback = getString(R.string.api_callback);
+			OAuthService service = new ServiceBuilder()
+					.provider(new RavelryApi(getString(R.string.ravelry_url)))
+					.apiKey(apiKey).apiSecret(apiSecret).callback(callback).build();
+
+			final Token accessToken = new Token(prefs.accessToken()
+					.get(), prefs.accessSecret().get());
+			final OAuthRequest request = new OAuthRequest(Verb.GET, String.format(
+					getString(R.string.ravelry_url) + "/projects/Jillda/list.json",
+					prefs.username().get()));
+			service.signRequest(accessToken, request);
+			request.setConnectTimeout(10, TimeUnit.SECONDS);
+			final Response response = request.send();
+			try {
+				return new Gson().fromJson(response.getBody(), ProjectResult.class);
+			} catch (JsonSyntaxException e) {
+				e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+				throw e;
+			}
+		}
+	}
+
+	class ProjectsListener implements RequestListener<ProjectResult> {
+
+		@Override
+		public void onRequestFailure(SpiceException spiceException) {
+			System.out.println("FAIL");
+		}
+
+		@Override
+		public void onRequestSuccess(ProjectResult result) {
+			System.out.println(result);
+		}
 	}
 }
---
+++ KnitDroid/src/de/vanmar/android/knitdroid/ravelry/dts/ProjectResult.java
@@ -0,0 +1,15 @@
+package de.vanmar.android.knitdroid.ravelry.dts;
+
+import java.util.List;
+
+/**
+ * User: Kolja
+ * Date: 11.11.13
+ * Time: 22:21
+ * <p/>
+ * TODO: Documentation
+ */
+public class ProjectResult {
+
+	List<Project> projects;
+}
