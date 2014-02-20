1aef6231f74f6f25fcd1181638b24f14a037b3af
Kolja Markwardt <vanmar.development@gmail.com>
2014-02-20 22:15:52 +0100

    bigger icon

--- resources/icons.bat
+++ resources/icons.bat
@@ -1,20 +1,20 @@
-convert -geometry 48x48 projects.png ../KnitDroid/res/drawable-hdpi/projects.png
-convert -geometry 32x32 projects.png ../KnitDroid/res/drawable-mdpi/projects.png
-convert -geometry 64x64 projects.png ../KnitDroid/res/drawable-xhdpi/projects.png
+convert -geometry 48x48 projects.png ../Yarrn/res/drawable-hdpi/projects.png
+convert -geometry 32x32 projects.png ../Yarrn/res/drawable-mdpi/projects.png
+convert -geometry 64x64 projects.png ../Yarrn/res/drawable-xhdpi/projects.png
 
-convert -geometry 48x48 login.png ../KnitDroid/res/drawable-hdpi/login.png
-convert -geometry 32x32 login.png ../KnitDroid/res/drawable-mdpi/login.png
-convert -geometry 64x64 login.png ../KnitDroid/res/drawable-xhdpi/login.png
+convert -geometry 48x48 login.png ../Yarrn/res/drawable-hdpi/login.png
+convert -geometry 32x32 login.png ../Yarrn/res/drawable-mdpi/login.png
+convert -geometry 64x64 login.png ../Yarrn/res/drawable-xhdpi/login.png
 
-convert -geometry 48x48 favorites.png ../KnitDroid/res/drawable-hdpi/favorites.png
-convert -geometry 32x32 favorites.png ../KnitDroid/res/drawable-mdpi/favorites.png
-convert -geometry 64x64 favorites.png ../KnitDroid/res/drawable-xhdpi/favorites.png
+convert -geometry 48x48 favorites.png ../Yarrn/res/drawable-hdpi/favorites.png
+convert -geometry 32x32 favorites.png ../Yarrn/res/drawable-mdpi/favorites.png
+convert -geometry 64x64 favorites.png ../Yarrn/res/drawable-xhdpi/favorites.png
 
-convert -geometry 48x48 logo.png ../KnitDroid/res/drawable-hdpi/logo.png
-convert -geometry 32x32 logo.png ../KnitDroid/res/drawable-mdpi/logo.png
-convert -geometry 64x64 logo.png ../KnitDroid/res/drawable-xhdpi/logo.png
+convert -geometry 48x48 logo.png ../Yarrn/res/drawable-hdpi/logo.png
+convert -geometry 32x32 logo.png ../Yarrn/res/drawable-mdpi/logo.png
+convert -geometry 64x64 logo.png ../Yarrn/res/drawable-xhdpi/logo.png
 
-convert -geometry 48x48 mail.png ../KnitDroid/res/drawable-hdpi/mail.png
-convert -geometry 32x32 mail.png ../KnitDroid/res/drawable-mdpi/mail.png
-convert -geometry 64x64 mail.png ../KnitDroid/res/drawable-xhdpi/mail.png
+convert -geometry 48x48 mail.png ../Yarrn/res/drawable-hdpi/mail.png
+convert -geometry 32x32 mail.png ../Yarrn/res/drawable-mdpi/mail.png
+convert -geometry 64x64 mail.png ../Yarrn/res/drawable-xhdpi/mail.png
 
