81c0ef047cc44e013cddf8f0f7f3dc818bf0682e
Kolja Markwardt <vanmar.development@gmail.com>
2014-03-15 18:03:21 +0100

    added icons

--- resources/icons.bat
+++ resources/icons.bat
@@ -1,3 +1,11 @@
+convert -geometry 72x72 logo.png ../Yarrn/res/drawable-hdpi/logo.png
+convert -geometry 48x48 logo.png ../Yarrn/res/drawable-mdpi/logo.png
+convert -geometry 96x96 logo.png ../Yarrn/res/drawable-xhdpi/logo.png
+
+convert -geometry 36x36 menu.png ../Yarrn/res/drawable-hdpi/menu.png
+convert -geometry 24x24 menu.png ../Yarrn/res/drawable-mdpi/menu.png
+convert -geometry 48x48 menu.png ../Yarrn/res/drawable-xhdpi/menu.png
+
 convert -geometry 48x48 projects.png ../Yarrn/res/drawable-hdpi/projects.png
 convert -geometry 32x32 projects.png ../Yarrn/res/drawable-mdpi/projects.png
 convert -geometry 64x64 projects.png ../Yarrn/res/drawable-xhdpi/projects.png
@@ -10,11 +18,68 @@ convert -geometry 48x48 favorites.png ../Yarrn/res/drawable-hdpi/favorites.png
 convert -geometry 32x32 favorites.png ../Yarrn/res/drawable-mdpi/favorites.png
 convert -geometry 64x64 favorites.png ../Yarrn/res/drawable-xhdpi/favorites.png
 
-convert -geometry 48x48 logo.png ../Yarrn/res/drawable-hdpi/logo.png
-convert -geometry 32x32 logo.png ../Yarrn/res/drawable-mdpi/logo.png
-convert -geometry 64x64 logo.png ../Yarrn/res/drawable-xhdpi/logo.png
+convert -geometry 48x48 stashes.png ../Yarrn/res/drawable-hdpi/stashes.png
+convert -geometry 32x32 stashes.png ../Yarrn/res/drawable-mdpi/stashes.png
+convert -geometry 64x64 stashes.png ../Yarrn/res/drawable-xhdpi/stashes.png
 
 convert -geometry 48x48 mail.png ../Yarrn/res/drawable-hdpi/mail.png
 convert -geometry 32x32 mail.png ../Yarrn/res/drawable-mdpi/mail.png
 convert -geometry 64x64 mail.png ../Yarrn/res/drawable-xhdpi/mail.png
+
+convert -geometry 48x48 settings.png ../Yarrn/res/drawable-hdpi/settings.png
+convert -geometry 32x32 settings.png ../Yarrn/res/drawable-mdpi/settings.png
+convert -geometry 64x64 settings.png ../Yarrn/res/drawable-xhdpi/settings.png
+
+convert -geometry 48x48 arrowleft.png ../Yarrn/res/drawable-hdpi/arrowleft.png
+convert -geometry 32x32 arrowleft.png ../Yarrn/res/drawable-mdpi/arrowleft.png
+convert -geometry 64x64 arrowleft.png ../Yarrn/res/drawable-xhdpi/arrowleft.png
+
+convert -geometry 48x48 arrowleft_selected.png ../Yarrn/res/drawable-hdpi/arrowleft_selected.png
+convert -geometry 32x32 arrowleft_selected.png ../Yarrn/res/drawable-mdpi/arrowleft_selected.png
+convert -geometry 64x64 arrowleft_selected.png ../Yarrn/res/drawable-xhdpi/arrowleft_selected.png
+
+convert -geometry 48x48 arrowright.png ../Yarrn/res/drawable-hdpi/arrowright.png
+convert -geometry 32x32 arrowright.png ../Yarrn/res/drawable-mdpi/arrowright.png
+convert -geometry 64x64 arrowright.png ../Yarrn/res/drawable-xhdpi/arrowright.png
+
+convert -geometry 48x48 arrowright_selected.png ../Yarrn/res/drawable-hdpi/arrowright_selected.png
+convert -geometry 32x32 arrowright_selected.png ../Yarrn/res/drawable-mdpi/arrowright_selected.png
+convert -geometry 64x64 arrowright_selected.png ../Yarrn/res/drawable-xhdpi/arrowright_selected.png
+
+
+convert -geometry 48x48 arrowsleft.png ../Yarrn/res/drawable-hdpi/arrowsleft.png
+convert -geometry 32x32 arrowsleft.png ../Yarrn/res/drawable-mdpi/arrowsleft.png
+convert -geometry 64x64 arrowsleft.png ../Yarrn/res/drawable-xhdpi/arrowsleft.png
+
+convert -geometry 48x48 arrowsleft_selected.png ../Yarrn/res/drawable-hdpi/arrowsleft_selected.png
+convert -geometry 32x32 arrowsleft_selected.png ../Yarrn/res/drawable-mdpi/arrowsleft_selected.png
+convert -geometry 64x64 arrowsleft_selected.png ../Yarrn/res/drawable-xhdpi/arrowsleft_selected.png
+
+convert -geometry 48x48 arrowsright.png ../Yarrn/res/drawable-hdpi/arrowsright.png
+convert -geometry 32x32 arrowsright.png ../Yarrn/res/drawable-mdpi/arrowsright.png
+convert -geometry 64x64 arrowsright.png ../Yarrn/res/drawable-xhdpi/arrowsright.png
+
+convert -geometry 48x48 arrowsright_selected.png ../Yarrn/res/drawable-hdpi/arrowsright_selected.png
+convert -geometry 32x32 arrowsright_selected.png ../Yarrn/res/drawable-mdpi/arrowsright_selected.png
+convert -geometry 64x64 arrowsright_selected.png ../Yarrn/res/drawable-xhdpi/arrowsright_selected.png
+
+convert -geometry 36x36 smiley1.png ../Yarrn/res/drawable-hdpi/smiley1.png
+convert -geometry 24x24 smiley1.png ../Yarrn/res/drawable-mdpi/smiley1.png
+convert -geometry 48x48 smiley1.png ../Yarrn/res/drawable-xhdpi/smiley1.png
+
+convert -geometry 36x36 smiley2.png ../Yarrn/res/drawable-hdpi/smiley2.png
+convert -geometry 24x26 smiley2.png ../Yarrn/res/drawable-mdpi/smiley2.png
+convert -geometry 48x48 smiley2.png ../Yarrn/res/drawable-xhdpi/smiley2.png
+
+convert -geometry 36x36 smiley3.png ../Yarrn/res/drawable-hdpi/smiley3.png
+convert -geometry 24x24 smiley3.png ../Yarrn/res/drawable-mdpi/smiley3.png
+convert -geometry 48x48 smiley3.png ../Yarrn/res/drawable-xhdpi/smiley3.png
+
+convert -geometry 36x36 smiley4.png ../Yarrn/res/drawable-hdpi/smiley4.png
+convert -geometry 24x24 smiley4.png ../Yarrn/res/drawable-mdpi/smiley4.png
+convert -geometry 48x48 smiley4.png ../Yarrn/res/drawable-xhdpi/smiley4.png
+
+convert -geometry 36x36 smiley5.png ../Yarrn/res/drawable-hdpi/smiley5.png
+convert -geometry 24x24 smiley5.png ../Yarrn/res/drawable-mdpi/smiley5.png
+convert -geometry 48x48 smiley5.png ../Yarrn/res/drawable-xhdpi/smiley5.png
 
